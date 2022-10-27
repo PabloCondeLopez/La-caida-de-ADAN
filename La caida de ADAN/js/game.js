80c71ae6125494cea36ccc1b2da66b30892ada49
David Antonio Vélez Rebollo <david_velezreb@hotmail.com>
2022-10-27 13:51:47 +0200

    Menu placeholder
    Primer vistazo al menu con Phaser

--- La caida de ADAN/js/game.js
+++ La caida de ADAN/js/game.js
@@ -1,33 +1,16 @@
-let path;
+import MainMenu from "./Scenes/MainMenu.js";
 
-function preload(){
+const gameWidth = 1280;
+const gameHeight = 720;
 
-}
-
-function create() {
-    
-}
-
-function update() {
-        
-}
+let mainMenu = new MainMenu('MainMenu');
 
 const config = {
     type: Phaser.AUTO,
-    width: 640,
-    height: 512,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300 },
-            debug: false
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    width: gameWidth,
+    height: gameHeight,
 };
 
 let game = new Phaser.Game(config);
+game.scene.add('MainMenu', mainMenu);
+game.scene.start('MainMenu');
