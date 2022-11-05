fd6bece64042d5ad1e53f05d7e3bba60ded23760
PabloCondeLopez <pabloconde112002@gmail.com>
2022-11-05 19:15:18 +0100

    Torretas deberian funcionar asi de pana

--- La caida de ADAN/js/turret.js
+++ La caida de ADAN/js/turret.js
@@ -1,12 +1,14 @@
 class Turret extends Phaser.GameObjects.Image {
     
     
-    constructor (scene){
-        var damage = this.damage;
+    constructor (scene, damage, attackRange){
+        var damage = damage;
+        var enemy = null;
+        var attackRange = attackRange;
         Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'turret');
         this.nextTic = 0;
     }
-
+    
     place(i, j){
         this.y = i*step;
         this.x = j*step;
@@ -14,13 +16,34 @@ class Turret extends Phaser.GameObjects.Image {
     }
 
     fire (){
-        var enemy = getEnemy(this.x, this.y, attackRange)
-        if(enemy) {
+        
+        if(enemy.isAlive() && (Math.abs(enemy.position - this.position) <= this.attackRange)) {
             var angle = Phaser.Math.Angle.Between(this.x, this.y, enemy.x, enemy.y);
             addBullet(this.x, this.y, angle);
             this.angle = (angle + Math.PI/2) * Phaser.Math.RAD_TO_DEG;
+            enemy.damage(this.damage);
+        }
+        else{
+            enemy = getEnemy(enemyList);
         }
     }
+
+    getEnemy(enemyList){
+       for(let i=0; i<enemyList.length; i++){
+        if(!this.enemyValid(this.enemy) && this.enemyValid(enemyList[i])){
+            enemy = enemyList[i];
+        }
+       }
+    }
+
+    enemyValid(enemy){
+        if(enemy.isAlive() && (Math.abs(enemy.position - this.position) <= this.attackRange)){
+            return true;
+        }
+        else
+        return false;
+    }
+
     update(time, delta)
     {
         if(time > this.nextTic) {
