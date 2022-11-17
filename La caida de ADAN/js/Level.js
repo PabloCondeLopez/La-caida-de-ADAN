4309adaace97a02c3a0a1fe55f57832618fac912
IceHummingBird <laura.garcs.marti@gmail.com>
2022-11-17 12:38:26 +0100

    Level & Player

--- La caida de ADAN/js/Level.js
+++ La caida de ADAN/js/Level.js
@@ -1,18 +1,38 @@
 class Level extends Phaser.GameObjects.Image {
 
-    constructor (x, y) {
+    constructor (num) {
+
+        this.level = 0;
         Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'firstMap');
+
+        // interfaz general
         Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'interface');
 
+        // skills
         Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'skill1');
-        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'skill1');
+        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'skill2');
+        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'skill3');
 
-        this.posX = x;
-        this.posY = Y;
-        this.building;
-        this.full = false;
-        this.buildMenu = document.getElementById("cellButton");
-        this.buildMenu = document.getElementById("buildMenu");
+        // texto
+        game.add.text(100,50,"Energia 1",{
+            font: 'Agency FB',
+            fontSize: 32,
+            fill: 'purple'
+        });
+
+        game.add.text(100,50,"Energia 2",{
+            font: 'Agency FB',
+            fontSize: 32,
+            fill: 'purple'
+        });
+
     }
+
+    updateEnergy(){
+        
+    }
+
+
+
 
 }
