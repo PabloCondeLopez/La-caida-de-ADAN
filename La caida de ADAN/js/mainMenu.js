0838127bf16ec6d10ecb62b18efc0559f51e73d4
David Antonio Vélez Rebollo <david_velezreb@hotmail.com>
2022-11-29 20:01:51 +0100

    Actualizado menú principal

--- La caida de ADAN/js/mainMenu.js
+++ La caida de ADAN/js/mainMenu.js
@@ -12,31 +12,28 @@ class MainMenu extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('background', 'assets/Fondo_placeholder.png');
+        this.load.image('background', 'assets/menu_principal.png');
         this.load.image('button', 'assets/boton_menu_principal.png');
-        this.load.image('logo', 'assets/logo.png');
     }
 
     create() {
         this.add.image(this.screenWidth / 2, this.screenHeight / 2, 'background');
 
-        this.add.image(150, 150, 'logo').setScale(0.3);
+        this.historyButton = this.add.image(this.screenWidth / 2 + 450, this.screenHeight / 2 - 250, 'button').setScale(3.5);
+        this.competitiveButton = this.add.image(this.screenWidth / 2 + 450, this.screenHeight / 2 - 50, 'button').setScale(3.5);
+        this.endlessButton = this.add.image(this.screenWidth / 2 + 450, this.screenHeight / 2 + 150, 'button').setScale(3.5);
 
-        this.historyButton = this.add.image(this.screenWidth / 2, this.screenHeight / 2 + 105, 'button').setScale(3.5);
-        this.competitiveButton = this.add.image(this.screenWidth / 2, this.screenHeight / 2 + 250, 'button').setScale(3.5);
-        this.endlessButton = this.add.image(this.screenWidth / 2, this.screenHeight / 2 + 400, 'button').setScale(3.5);
-
-        this.historyText = this.add.text(this.screenWidth / 2 + 5, this.screenHeight / 2 + 100, 'Historia', {fontSize: '40px', fill: '#fff', fontFamily: 'Pixeled'}).setStroke("#000", 4).setOrigin(0.5, 0.5);
+        this.historyText = this.add.text(this.screenWidth / 2 + 455, this.screenHeight / 2 - 255, 'Historia', {fontSize: '40px', fill: '#fff', fontFamily: 'Pixeled'}).setStroke("#000", 4).setOrigin(0.5, 0.5);
         this.historyText.setInteractive();
         this.historyText.on('pointerdown', this.startLevel);
 
-        this.competitiveText = this.add.text(this.screenWidth / 2 + 2, this.screenHeight / 2 + 245, 'Competitivo', {fontSize: '29px', fill: '#fff', fontFamily: 'Pixeled'}).setStroke("#000", 4).setOrigin(0.5, 0.5);
-        this.endlessText = this.add.text(this.screenWidth / 2 + 5, this.screenHeight / 2 + 395, 'Infinito', {fontSize: '40px', fill: '#fff', fontFamily: 'Pixeled'}).setStroke('#000', 4).setOrigin(0.5, 0.5);
+        this.competitiveText = this.add.text(this.screenWidth / 2 + 452, this.screenHeight / 2 - 55, 'Competitivo', {fontSize: '29px', fill: '#fff', fontFamily: 'Pixeled'}).setStroke("#000", 4).setOrigin(0.5, 0.5);
+        this.endlessText = this.add.text(this.screenWidth / 2 + 455, this.screenHeight / 2 + 145, 'Infinito', {fontSize: '40px', fill: '#fff', fontFamily: 'Pixeled'}).setStroke('#000', 4).setOrigin(0.5, 0.5);
     }
 
     startLevel(){
-        game.scene.stop('MainMenu')
-        game.scene.start('Level')
+        game.scene.stop('MainMenu');
+        game.scene.start('Level');
     }
 }
 
