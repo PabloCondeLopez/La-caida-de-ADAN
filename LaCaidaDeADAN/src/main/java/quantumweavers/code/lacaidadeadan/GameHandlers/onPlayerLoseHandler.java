f8cdc9daf04f1f6779e8e10e04298206a833f2b7
David Antonio Vélez Rebollo <david_velezreb@hotmail.com>
2022-12-28 18:38:16 +0100

    WebSockets Handlers creation

---
+++ LaCaidaDeADAN/src/main/java/quantumweavers/code/lacaidadeadan/GameHandlers/onPlayerLoseHandler.java
@@ -0,0 +1,32 @@
+package quantumweavers.code.lacaidadeadan.GameHandlers;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class onPlayerLoseHandler extends TextWebSocketHandler {
+	private Map<String, WebSocketSession> userSessions = new ConcurrentHashMap<>();
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		for(WebSocketSession user : userSessions.values()) {
+			if(user.getId().equals(session.getId())) continue;
+			
+			user.sendMessage(new TextMessage(message.getPayload()));
+		}
+	}
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		userSessions.put(session.getId(), session);
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		userSessions.remove(session.getId());
+	}
+}
